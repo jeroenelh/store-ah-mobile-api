013a6dff827dd504733a64d9a321f19b9eeee007
Jeroen el Hahaoui <j.el.hahaoui@micro-it.nl>
2023-05-19 23:39:45 +0200

    Added models

--- src/AH.php
+++ src/AH.php
@@ -2,9 +2,47 @@
 
 namespace Microit\StoreAhApi;
 
+use Microit\StoreAhApi\Models\Category;
+use Microit\StoreAhApi\Models\Image;
+
 class AH
 {
-    public function getProduct() {
+    protected ClientConnection $clientConnection;
+    public function __construct()
+    {
+        $connection = ClientConnection::getInstance();
+        assert($connection instanceof ClientConnection);
+        $this->clientConnection = $connection;
+    }
 
+    public function getCategories(): array
+    {
+        $request = $this->clientConnection->createRequest('get', 'mobile-services/v1/product-shelves/categories');
+        $response = $this->clientConnection->getJsonResponse($request);
+
+        $categories = [];
+        /** @var object $categoryResponse */
+        foreach ($response as $categoryResponse) {
+            assert(is_int($categoryResponse->id));
+            assert(is_string($categoryResponse->name));
+            assert(is_string($categoryResponse->slugifiedName));
+            assert(is_array($categoryResponse->images) && count($categoryResponse->images) && is_object($categoryResponse->images[0]));
+            assert(is_string($categoryResponse->images[0]->url));
+            assert(is_int($categoryResponse->images[0]->width));
+            assert(is_int($categoryResponse->images[0]->height));
+
+            $categories[] = (new Category(
+                id: $categoryResponse->id,
+                name: $categoryResponse->name,
+                slug: $categoryResponse->slugifiedName,
+                image: (new Image(
+                    url: $categoryResponse->images[0]->url,
+                    width: $categoryResponse->images[0]->width,
+                    height: $categoryResponse->images[0]->height)
+                )
+            ));
+        }
+
+        return $categories;
     }
 }
--- src/ClientConnection.php
+++ src/ClientConnection.php
@@ -20,12 +20,12 @@ class ClientConnection extends \Microit\StoreBase\HttpClient
 
 //        $request = $this->createRequest('get', 'mobile-services/product/detail/v4/fir/wi233440');
 //        $request = $this->createRequest('get', 'mobile-services/bonuspage/v1/metadata');
-        $request = $this->createRequest('get', 'mobile-services/v1/product-shelves/categories');
+//        $request = $this->createRequest('get', 'mobile-services/v1/product-shelves/categories');
 //        $request = $this->createRequest('get', '/mobile-services/product/search/v2');
 //        $request = $this->createRequest('get', '/mobile-services/product/search/v2?page=0&query=bier&sortOn=RELEVANCE');
 //        $request = $request->withBody(Stream::create(json_encode(['category' => 'Diepvries'])));
 
-        var_dump($this->getJsonResponse($request));
+//        var_dump($this->getJsonResponse($request));
     }
 
     public function createRequest(string $method, string $uri): RequestInterface
@@ -40,6 +40,11 @@ class ClientConnection extends \Microit\StoreBase\HttpClient
         return $request;
     }
 
+    /**
+     * @param RequestInterface $request
+     * @return object|array<array-key, mixed>
+     * @throws \Psr\Http\Client\ClientExceptionInterface
+     */
     public function getJsonResponse(RequestInterface $request): object|array
     {
         $response = $this->getResponse($request);
@@ -47,6 +52,10 @@ class ClientConnection extends \Microit\StoreBase\HttpClient
         $jsonResponse = json_decode($response->getBody()->getContents());
         if (is_null($jsonResponse)) {
             throw new \Exception("Can't convert to json");
+        }
+
+        if (!(is_object($jsonResponse) || is_array($jsonResponse))) {
+            throw new \Exception("Bad json response");
         }
 
         return $jsonResponse;
